a642e339cdb04c21c2a2cf738659011800d7116e
jaebooker <jaesonbooker@gmail.com>
2019-04-25 23:57:17 -0700

    attempted moving to check if possible

--- engine.py
+++ engine.py
@@ -9,6 +9,25 @@ import chess
 #         self.repeats = repeats
 #         self.previous_move = previous_move
 # repeats = Repeats(0,None)
+def check(board, move) -> bool:
+    king = board.king(not board.turn)
+    return king is not None and board.is_attacked_by(board.turn, king)
+def enemy_into_check(board, move) -> bool:
+        """
+        Checks if the given move would leave the king in check or put it into
+        check. The move must be at least pseudo legal.
+        """
+        king = board.king(not board.turn)
+        if king is None:
+            return False
+
+        checkers = board.attackers_mask(board.turn, king)
+        if checkers:
+            # If already in check, look if it is an evasion.
+            if move not in board._generate_evasions(checkers,king,BB_SQUARES[move.from_square], BB_SQUARES[move.to_square]):
+                return True
+
+        return not board._is_safe(king, board._slider_blockers(king), move)
 def get_move(board, limit=None):
   # TODO: Fill this in with an actual chess engine
   # choice = list(board.legal_moves)
@@ -20,6 +39,11 @@ def get_move(board, limit=None):
   counter = 0
   while counter < 100:
       move = random.choice(list(board.legal_moves))
+      uci = board.uci(move)
+      print(uci)
+      copy = enemy_into_check(board, move)
+      if copy == True:
+          return move
       if board.is_capture(move):
           # if move == repeats.previous_move:
           #     if repeats.repeat == 3:
